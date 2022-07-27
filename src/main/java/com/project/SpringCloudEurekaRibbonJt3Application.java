e02038d62bb96bb1300b9007c34958d734c44f74
Emerson Millard <emillard.code@gmail.com>
2022-07-27 12:00:00 -0700

    Added Eureka annotation to main class.

--- src/main/java/com/project/SpringCloudEurekaRibbonJt3Application.java
+++ src/main/java/com/project/SpringCloudEurekaRibbonJt3Application.java
@@ -1,13 +1,17 @@
-package com.project.springcloudeurekaribbonjt3;
+package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringCloudEurekaRibbonJt3Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringCloudEurekaRibbonJt3Application.class, args);
+
     }
 
 }
