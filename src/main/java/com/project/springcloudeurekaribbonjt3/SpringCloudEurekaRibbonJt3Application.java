0f28b3486f61dbd9227e8ec32f73a4b420037033
Emerson Millard <emillard.code@gmail.com>
2022-08-11 06:25:25 -0700

    Initial commit

---
+++ src/main/java/com/project/springcloudeurekaribbonjt3/SpringCloudEurekaRibbonJt3Application.java
@@ -0,0 +1,13 @@
+package com.project.springcloudeurekaribbonjt3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaRibbonJt3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaRibbonJt3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/springcloudeurekaribbonjt3/SpringCloudEurekaRibbonJt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springcloudeurekaribbonjt3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaRibbonJt3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
